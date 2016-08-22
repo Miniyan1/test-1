534101799e2ea125e113816143391585414e38da
Ashwin Rana <ashwinrana10@gmail.com>
2016-08-22 23:08:54 +0545

    Update db.php

--- project/include/db.php
+++ project/include/db.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$dbname = "project";
+$host = " "; //host name usually if you are using local machine then it will be "localhost".
+$user = " "; //username usually if you are using local machine then it will be "root".
+$pass = ""; //username usually if you are using local machine then it will be blank.
+$dbname = "project"; //it is the database name where your related table are located. In my case it is stored in project database and user table.
 
 $conn = mysqli_connect($host,$user,$pass,$dbname);
 
